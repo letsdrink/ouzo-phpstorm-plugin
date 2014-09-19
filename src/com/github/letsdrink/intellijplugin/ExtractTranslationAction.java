0e024e0d5445a163613bc455c6f224c73fff7e14
woru <wor18@wp.pl>
2014-09-19 16:25:08 +0200

    extracting translations - work in progress

---
+++ src/com/github/letsdrink/intellijplugin/ExtractTranslationAction.java
@@ -0,0 +1,125 @@
+package com.github.letsdrink.intellijplugin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.application.Result;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.search.FilenameIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.xml.XmlTokenType;
+
+public class ExtractTranslationAction extends AnAction {
+    public ExtractTranslationAction() {
+        super("ExtractTranslation");
+    }
+
+    public void actionPerformed(AnActionEvent event) {
+
+        final Editor editor = event.getData(PlatformDataKeys.EDITOR);
+        PsiFile psiFile = event.getData(LangDataKeys.PSI_FILE);
+        if (psiFile == null || editor == null)
+            return;
+
+        int offset = editor.getCaretModel().getOffset();
+        PsiElement psiElement = psiFile.findElementAt(offset);
+        if (psiElement == null || psiElement.getNode() == null)
+            return;
+
+        IElementType elementType = psiElement.getNode().getElementType();
+        if (!isTypeSupported(elementType)) {
+            return;
+        }
+        if (isTypeSupported(psiElement.getParent().getNode().getElementType())) {
+            psiElement = psiElement.getParent();
+        }
+        final PsiElement finalPsiElement = psiElement;
+
+
+        TranslationDialog dialog = new TranslationDialog("test", new TranslationDialog.OkCallback() {
+            @Override
+            public void onClick(final String key, final String plText, final String enText) {
+                replaceTextWithTranslation(key, finalPsiElement, editor);
+                addTranslation(key, enText, "en.php", finalPsiElement.getProject());
+                addTranslation(key, plText, "pl.php", finalPsiElement.getProject());
+            }
+        });
+        dialog.pack();
+        dialog.setTitle("Input translation key");
+        dialog.setSize(300,200);
+        dialog.setLocationRelativeTo(null);
+        dialog.setVisible(true);
+    }
+
+    private void addTranslation(String key, String enText, String langFile, final Project project) {
+        PsiFile enFile = getPsiFile(langFile, project);
+
+        final PsiDocumentManager manager = PsiDocumentManager.getInstance(project);
+        final Document document = manager.getDocument(enFile);
+
+        final String finalInsertString = "\n'" + key + "' => '" + enText + "',";
+        new WriteCommandAction(project) {
+            @Override
+            protected void run(Result result) throws Throwable {
+                document.insertString(document.getTextLength(), finalInsertString);
+                manager.doPostponedOperationsAndUnblockDocument(document);
+                manager.commitDocument(document);
+            }
+
+            @Override
+            public String getGroupID() {
+                return "Translation Extraction";
+            }
+        }.execute();
+    }
+
+    private void replaceTextWithTranslation(final String key, final PsiElement finalPsiElement, final Editor editor) {
+        PsiDocumentManager.getInstance(finalPsiElement.getProject()).doPostponedOperationsAndUnblockDocument(editor.getDocument());
+        final TextRange range = finalPsiElement.getTextRange();
+        new WriteCommandAction(finalPsiElement.getProject()) {
+            @Override
+            protected void run(Result result) throws Throwable {
+                String insertString = "<?= t('" + key + "')?>";
+
+                if (isJsLiteral(finalPsiElement.getNode().getElementType())) {
+                    insertString = "\"" + insertString + "\"";
+                }
+                editor.getDocument().replaceString(range.getStartOffset(), range.getEndOffset(), insertString);
+                editor.getCaretModel().moveToOffset(range.getStartOffset());
+            }
+
+            @Override
+            public String getGroupID() {
+                return "Translation Extraction";
+            }
+        }.execute();
+    }
+
+    private PsiFile getPsiFile(String langFile, Project project) {
+        PsiFile[] files = FilenameIndex.getFilesByName(project, langFile, GlobalSearchScope.allScope(project));
+        for (PsiFile file : files) {
+            if (!file.getVirtualFile().getCanonicalPath().contains("vendor")) {
+                return file;
+            }
+        }
+        return null;
+    }
+
+
+    private boolean isTypeSupported(IElementType elementType) {
+        return elementType == XmlTokenType.XML_DATA_CHARACTERS || elementType == XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN || elementType.toString().equals("XML_TEXT") || isJsLiteral(elementType);
+    }
+
+    private boolean isJsLiteral(IElementType elementType) {
+        return elementType.toString().equals("JS:STRING_LITERAL");
+    }
+}
---
+++ src/com/github/letsdrink/intellijplugin/TranslationDialog.java
@@ -0,0 +1,77 @@
+package com.github.letsdrink.intellijplugin;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class TranslationDialog extends JDialog {
+
+    private final OkCallback okCallback;
+
+    public interface OkCallback {
+        void onClick(String key, String plText, String enText);
+    }
+
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JTextField key;
+    private JTextField plText;
+    private JTextField enText;
+
+    public TranslationDialog(String text, OkCallback okCallback) {
+        this.okCallback = okCallback;
+        setContentPane(contentPane);
+        setModal(true);
+        plText.setText(text);
+        enText.setText(text);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+// call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+// call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK() {
+        okCallback.onClick(key.getText(), plText.getText(), enText.getText());
+        dispose();
+    }
+
+    private void onCancel() {
+// add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        TranslationDialog dialog = new TranslationDialog("test", new OkCallback() {
+            @Override
+            public void onClick(final String key, final String plText, final String enText) {
+            }
+        });
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
