18f99fffa205147201479b23bef16a6d98490531
woru <wor18@wp.pl>
2014-09-29 09:58:01 +0200

    fixed dynamic translations

--- src/com/github/letsdrink/intellijplugin/TranslationCallParser.java
+++ src/com/github/letsdrink/intellijplugin/TranslationCallParser.java
@@ -4,6 +4,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.Collection;
 
@@ -18,6 +19,7 @@ public class TranslationCallParser {
             if (TranslationUtils.isTranslationCall(call)) {
                 PsiElement psiElement = TranslationUtils.extractKey(call);
                 String key = PsiUtils.getContent(psiElement);
+                key = StringUtils.strip(key, ".");
                 if (key != null) {
                     handler.handleKey(key, psiElement);
                 }
