b8d208b6187db865471310d38aafacfacc9cdefc
woru <wor18@wp.pl>
2014-10-06 14:07:10 +0200

    fixed #37: rename renames dynamic usages

--- src/com/github/letsdrink/intellijplugin/translation/rename/RenameTranslationKeyUsageInfo.java
+++ src/com/github/letsdrink/intellijplugin/translation/rename/RenameTranslationKeyUsageInfo.java
@@ -4,18 +4,28 @@ package com.github.letsdrink.intellijplugin.translation.rename;
 import com.intellij.psi.PsiElement;
 import com.jetbrains.php.lang.parser.PhpElementTypes;
 import com.jetbrains.php.lang.psi.PhpPsiElementFactory;
+import com.jetbrains.php.lang.psi.elements.ParameterList;
 import org.jetbrains.annotations.NotNull;
 
 public class RenameTranslationKeyUsageInfo extends RenameTranslationUsageInfo {
+    private boolean refactor = true;
+
     public RenameTranslationKeyUsageInfo(@NotNull PsiElement element) {
         super(element);
+
+        if (!(element.getContext() instanceof ParameterList)) {
+            setDynamicUsage(true);
+            refactor = false;
+        }
     }
 
     @Override
     void performRefactoring(String newKey) {
-        PsiElement element = getElement();
-        PsiElement psiElement = PhpPsiElementFactory.createFromText(element.getProject(), PhpElementTypes.STRING, "'" + newKey + "'");
-        element.replace(psiElement);
+        if (refactor) {
+            PsiElement element = getElement();
+            PsiElement psiElement = PhpPsiElementFactory.createFromText(element.getProject(), PhpElementTypes.STRING, "'" + newKey + "'");
+            element.replace(psiElement);
+        }
     }
 
     @Override
